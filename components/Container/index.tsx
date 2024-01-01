11ef7658055fd50115e970487e691dca73161cee
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-01-01 15:05:51 +0700

    Add container component

---
+++ components/Container/index.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components/native";
+
+const Container = styled.View({
+  display: "flex",
+  flex: 1,
+  width: "100%",
+  height: "100%",
+  paddingTop: 24,
+  paddingBottom: 48,
+  paddingHorizontal: 24,
+});
+
+export default Container;
