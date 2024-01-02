abad152034d13b37608c081f807ed976cd0cea0c
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-01-03 00:03:23 +0700

    Add new component and update existing component

---
+++ app/(product)/add.tsx
@@ -0,0 +1,9 @@
+import { View } from "react-native";
+
+const AddProduct = () => {
+  return(
+    <View></View>
+  )
+};
+
+export default AddProduct;
--- components/NewItem/index.tsx
+++ components/NewItem/index.tsx
@@ -1,4 +1,5 @@
-import { View } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+import { Image, ImageSourcePropType, Text, View } from "react-native";
 
 export const NewsItem = [
   {
@@ -27,8 +28,54 @@ export const NewsItem = [
   },
 ];
 
-const NewItem = () => {
-  return <View></View>;
+const NewItem = ({
+  avatar,
+  name,
+  title,
+  description,
+  createAt,
+  repliesCount,
+}: {
+  avatar: ImageSourcePropType;
+  name: string;
+  title: string;
+  description: string;
+  createAt: string;
+  repliesCount: number;
+}) => {
+  return (
+    <View style={{ width: "100%" }}>
+      <View
+        style={{ width: "100%", flexDirection: "row", alignItems: "center" }}
+      >
+        <Image source={avatar} style={{ width: 27, height: 20 }} />
+        <Text style={{ fontSize: 18, fontWeight: "400", color: "#000" }}>
+          {name}
+        </Text>
+        <FontAwesome name="bookmark" size={20} color="black" />
+      </View>
+      <View style={{ gap: 12 }}>
+        <Text style={{ fontSize: 17, fontWeight: "700" }}>{title}</Text>
+        <Text style={{ fontSize: 17, fontWeight: "400", color: "#000" }}>
+          {description}
+        </Text>
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          width: "100%",
+        }}
+      >
+        <Text style={{ fontSize: 18, fontWeight: "400", color: "#989AA6" }}>
+          {createAt}
+        </Text>
+        <Text style={{ fontSize: 18, fontWeight: "400", color: "#000000" }}>
+          {repliesCount} Phản hồi
+        </Text>
+      </View>
+    </View>
+  );
 };
 
 export default NewItem;
